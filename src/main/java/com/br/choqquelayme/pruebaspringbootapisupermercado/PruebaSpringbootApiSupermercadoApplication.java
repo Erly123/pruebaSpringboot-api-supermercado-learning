3b27c72d0f0f6660f7f69aa05f0e8ae7f4261dfd
Erly123 <choquelayme@gmail.com>
2025-11-27 14:47:45 -0300

    first commit

---
+++ src/main/java/com/br/choqquelayme/pruebaspringbootapisupermercado/PruebaSpringbootApiSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package com.br.choqquelayme.pruebaspringbootapisupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringbootApiSupermercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaSpringbootApiSupermercadoApplication.class, args);
+    }
+
+}
