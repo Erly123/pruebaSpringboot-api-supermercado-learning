fe7ad19f5710ac4e66e15cd4e5a086b1908a756a
Erly123 <choquelayme@gmail.com>
2025-12-01 18:43:33 -0300

    feat: add method create, update, delete in ProductoService, SucursalService and VentaService

--- src/main/java/com/br/choqquelayme/pruebaspringbootapisupermercado/model/Venta.java
+++ src/main/java/com/br/choqquelayme/pruebaspringbootapisupermercado/model/Venta.java
@@ -1,10 +1,7 @@
 package com.br.choqquelayme.pruebaspringbootapisupermercado.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,6 +11,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
